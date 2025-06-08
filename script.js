0c60b4ae795b4a2373eb30e8678a5c132b6b9bbb
gab-code-killer <gabzcardon@gmail.com>
2025-06-08 10:54:45 +0200

    en construction pour les télephone réussi

--- script.js
+++ script.js
@@ -14,56 +14,82 @@ const boutonC = document.getElementById("boutonC");
 const boutonPoint = document.getElementById("boutonPoint");
 
 boutonUn.addEventListener("click", function () {
-    if (divText.innerText.length <= 14) {
+    if (divText.innerText.length <= 13) {
         divText.innerText += "1";
+    }
+        if (divText.innerText === "01") {
+        divText.innerText -= "0";
     }
 });
 boutonDeux.addEventListener("click", function () {
-    if (divText.innerText.length <= 14) {
+    if (divText.innerText.length <= 13) {
         divText.innerText += "2";
+    }
+        if (divText.innerText === "02") {
+        divText.innerText -= "0";
     }
 });
 boutonTrois.addEventListener("click", function () {
 
-    if (divText.innerText.length <= 14) {
+    if (divText.innerText.length <= 13) {
         divText.innerText += "3";
     }
-
+    if (divText.innerText === "03") {
+        divText.innerText -= "0";
+    }
 });
 boutonQuatre.addEventListener("click", function () {
-    if (divText.innerText.length <= 14) {
+    if (divText.innerText.length <= 13) {
         divText.innerText += "4";
+    }
+        if (divText.innerText === "04") {
+        divText.innerText -= "0";
     }
 });
 boutonCinq.addEventListener("click", function () {
-    if (divText.innerText.length <= 14) {
+    if (divText.innerText.length <= 13) {
         divText.innerText += "5";
+    }
+        if (divText.innerText === "05") {
+        divText.innerText -= "0";
     }
 });
 boutonSix.addEventListener("click", function () {
 
-    if (divText.innerText.length <= 14) {
+    if (divText.innerText.length <= 13) {
         divText.innerText += "6";
+    }
+        if (divText.innerText === "06") {
+        divText.innerText -= "0";
     }
 });
 boutonSept.addEventListener("click", function () {
-    if (divText.innerText.length <= 14) {
+    if (divText.innerText.length <= 13) {
         divText.innerText += "7";
+    }
+        if (divText.innerText === "07") {
+        divText.innerText -= "0";
     }
 });
 boutonHuit.addEventListener("click", function () {
-    if (divText.innerText.length <= 14) {
+    if (divText.innerText.length <= 13) {
         divText.innerText += "8";
+    }
+        if (divText.innerText === "08") {
+        divText.innerText -= "0";
     }
 });
 boutonNeuf.addEventListener("click", function () {
-    if (divText.innerText.length <= 14) {
+    if (divText.innerText.length <= 13) {
         divText.innerText += "9";
+    }
+    if (divText.innerText === "09") {
+        divText.innerText -= "0";
     }
 
 });
 boutonZero.addEventListener("click", function () {
-    if (divText.innerText.length <= 14 && divText.innerText.length > 0) {
+    if (divText.innerText.length <= 13) {
         divText.innerText += "0";
     }
 });
@@ -71,7 +97,7 @@ boutonC.addEventListener("click", function () {
     divText.innerText = "";
 });
 boutonPoint.addEventListener("click", function () {
-    if (divText.innerText.length <= 14 && divText.innerText.length > 0 && divText.innerText.includes(".") === false) {
+    if (divText.innerText.length <= 13 && divText.innerText.length > 0 && divText.innerText.includes(".") === false) {
         divText.innerText += ".";
     }
 });
